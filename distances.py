3eaa8027e7caa521caf7dc9e7674e5b2dfe517d0
Dakota Hawkins <dakotahawkins0110@gmail.com>
2019-07-30 10:38:47 -0400

    Added numba accelerated L1 and L2 distances.

--- distances.py
+++ distances.py
@@ -2,6 +2,23 @@ import numpy as np
 import numba
 
 _mock_ones = np.ones(2, dtype=np.float64)
+# implementation/syntax inspired by UMAP.distances
+
+@numba.njit()
+def manhattan(x, y, w=_mock_ones):
+    """Calculate the L1-distance between two vectors."""
+    result = 0.0
+    for i in range(x.shape[0]):
+        result += w[i] * abs(x[i] - y[i])
+    return result
+
+@numba.njit()
+def sqeuclidean(x, y, w=_mock_ones):
+    """Calculate the L2-distance between two vectors."""
+    result = 0.0
+    for i in range(x.shape[0]):
+        result += w[i] * (x[i] - y[i]) ** 2
+    return result
 
 @numba.njit()
 def variance(x, w=_mock_ones):
@@ -20,5 +37,20 @@ def variance(x, w=_mock_ones):
 
 @numba.njit()
 def phi_s(x, y, w=_mock_ones):
-    """Calculate the phi_s proportionaly metric between two vectors."""
+    """Calculate the phi_s proportionality metric between two vectors."""
     return variance(x - y, w) / variance(x + y, w)
+
+@numba.njit()
+def rho_p(x, y, w=_mock_ones):
+    """Calculate the rho_p proportionality metric between two vectors."""
+    result = 
+    return(variance(x - y, w) / variance(x, w) + variance(y, w))
+  
+supported_distances = {'l1': manhattan,
+                       'cityblock': manhattan,
+                       'taxicab': manhattan,
+                       'manhattan': manhattan,
+                       'l2': sqeuclidean,
+                       'sqeuclidean': sqeuclidean,
+                       'phi_s': phi_s,
+                       'rho_p': rho_p}
